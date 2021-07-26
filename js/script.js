d960e300dda2da04126ec2c7a889a254154221cd
Denis <logunovich@gmail.com>
2021-07-26 19:19:33 +0200

    first test

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Решение таймера по уроку:
 
-const deadLine = '2021-09-08'; 
+const deadLine = '2021-10-08'; 
 
 function getTimeRemaning (endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
