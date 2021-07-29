d3f8baf837f9f01738ea0fedc0176126c1dd2e25
Denis <logunovich@gmail.com>
2021-07-29 19:01:40 +0200

    my commit

--- js/script.js
+++ js/script.js
@@ -182,6 +182,7 @@ function openModal() {
     modal.classList.remove('hide');
     document.body.style.overflow = 'hidden';
     clearInterval(modalTimerId);
+    window.removeEventListener('scroll', showModalByScroll); // убираем обработчик со скрола
 }
 
 function closeModal() {
@@ -209,7 +210,7 @@ document.addEventListener('keydown', (e) => {
 
 // дорабатываем модельное окно
 
-const modalTimerId = setTimeout (openModal, 1500);
+const modalTimerId = setTimeout (openModal, 150000);
 
 function showModalByScroll () {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -218,10 +219,42 @@ function showModalByScroll () {
     }
 }
 
-
-
-
 window.addEventListener('scroll', showModalByScroll);
 
+// конец доработки модульного окна
+
+// Выводим динамически элементы на страницу при помощи классов и конструкторов. Мой вариант (31 строка): 
+
+
+const item = document.querySelectorAll('.menu__item');
+console.log(item);
+
+class DayMenu {
+    constructor(img, alt, title, descr, price) {
+        this.img = img;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price * 13;
+    }
+    embedContentMenu() {
+        return `<img src="${this.img}" alt="${this.alt}">
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>`
+    }
+}
+
+const menu1 = new DayMenu('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 29).embedContentMenu();
+const menu2 = new DayMenu('img/tabs/elite.jpg', 'elite', 'Меню "Премиум"', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 55).embedContentMenu();
+const menu3 = new DayMenu('img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ', 40).embedContentMenu();
+
+item[0].innerHTML = menu1;
+item[1].innerHTML = menu2;
+item[2].innerHTML = menu3;
 
 });
